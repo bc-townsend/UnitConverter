3227fb237ff20deb1aad50119b581b123b916b98
Brandon Townsend <bc_townsend@outlook.com>
2025-05-05 16:05:19 -0400

    Base Unit Converter UI
    Basic unit converter UI code is in place. More to come in a future PR.

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +36,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             UnitConverterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Modifier.padding(innerPadding)
+                    UnitConverter()
                 }
             }
         }
@@ -31,17 +45,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UnitConverter() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Unit Converter")
+        Spacer(Modifier.height(16.dp))
+        OutlinedTextField(value = "", onValueChange = {})
+        Spacer(Modifier.height(16.dp))
+
+        Row {
+            UnitDropdownMenu()
+            Spacer(Modifier.width(16.dp))
+            UnitDropdownMenu()
+        }
+
+        Spacer(Modifier.height(16.dp))
+        Text("Result: ")
+    }
+}
+
+@Composable
+fun UnitDropdownMenu() {
+    Box {
+        Button(onClick = {}) {
+            Text("Select")
+            Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+        }
+        DropdownMenu(expanded = false, onDismissRequest = {}) {
+            DropdownMenuItem(
+                text = { Text("Centimeters") },
+                onClick  = {}
+            )
+            DropdownMenuItem(
+                text = { Text("Meters") },
+                onClick = {}
+            )
+            DropdownMenuItem(
+                text = { Text("Feet") },
+                onClick = {}
+            )
+            DropdownMenuItem(
+                text = { Text("Inches") },
+                onClick = {}
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview() {
+    UnitConverter()
 }
